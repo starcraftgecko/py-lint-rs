a84317548262e7ce9a417db8e6504e2b4400577b
josh <joshuasalter1@gmail.com>
2026-09-19 00:44:43 -0500

    Commit verified F401+E711+B006 combined auto-fix result on needs_fix_triple.py

--- examples/needs_fix_triple.py
+++ examples/needs_fix_triple.py
@@ -1,10 +1,11 @@
-import os
 import sys as system
 
 
-def get_value(x, cache={}):
+def get_value(x, cache=None):
     """Return the cached value for x, or a sentinel if x is None."""
-    if x == None:
+    if cache is None:
+        cache = {}
+    if x is None:
         return "missing"
     return cache.get(x)
 
